771bf80e50518b38007ddcae9651c5b75fe3d768
simonkimi <805757448@qq.com>
2020-03-07 15:34:27 +0800

    新增 请求后台不休眠权限

--- app/src/main/java/moe/protector/pe/activity/LoginActivity.java
+++ app/src/main/java/moe/protector/pe/activity/LoginActivity.java
@@ -66,6 +66,10 @@ public class LoginActivity extends AppCompatActivity {
                 new SweetAlertDialog(LoginActivity.this)
                         .setTitleText("出错了!")
                         .setContentText((String) msg.obj)
+                        .setConfirmClickListener(sweetAlertDialog -> {
+                            loginAlertDialog.cancel();
+                            sweetAlertDialog.cancel();
+                        })
                         .show();
                 break;
             case LOGIN_SHOW_SERVER:  // 登录成功
